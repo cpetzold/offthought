b165ab50d9a59fb9d4b75e4c9e8c96abaed66187
Conner Petzold <cpetzold@gmail.com>
2009-07-21 10:08:25 -0700

    Started styling and switched to jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+Application = function() {
+  
+  return {
+    
+    init: function() {
+      var self = Application;
+  
+      $('#container').corners('10px');
+      $('#menu li a').corners('5px');
+  
+    }
+    
+  };
+  
+}();
+
+$(Application.init);
